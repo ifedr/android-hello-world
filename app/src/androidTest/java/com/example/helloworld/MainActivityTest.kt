727a717860c0ab84e3a80ace817e6994a22b8349
Sergei <imfedr@gmail.com>
2020-04-03 14:34:35 +0300

    Add UI test

---
+++ app/src/androidTest/java/com/example/helloworld/MainActivityTest.kt
@@ -0,0 +1,26 @@
+package com.example.helloworld
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        onView(withId(R.id.my_view)).check(matches(withText("Hello World!")))
+    }
+}
