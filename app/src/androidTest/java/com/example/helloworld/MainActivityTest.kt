09f55ec846a30c473f8611b1f457fbea62b20b31
Sergei <imfedr@gmail.com>
2020-04-03 15:25:23 +0300

    Add reportHelper

--- app/src/androidTest/java/com/example/helloworld/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/helloworld/MainActivityTest.kt
@@ -3,13 +3,17 @@ package com.example.helloworld
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import com.microsoft.appcenter.espresso.Factory
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -18,6 +22,15 @@ class MainActivityTest {
     @Rule
     @JvmField
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Rule
+    @JvmField
+    var reportHelper = Factory.getReportHelper()
+
+    @After
+    fun TearDown() {
+        reportHelper.label("Stopping App")
+    }
 
     @Test
     fun mainActivityTest() {
